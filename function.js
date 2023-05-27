f71cc60a6a58e1613f25971eb20c9391456fdb13
SnehaG24 <snehagharal11@gmail.com>
2023-05-27 15:18:29 +0530

    updated reset button again

--- function.js
+++ function.js
@@ -7,13 +7,9 @@ function next() {
     var eeb = parseInt(document.getElementById('inp4').value);
     var pce = parseInt(document.getElementById('inp5').value);
 
-
     var add = document.getElementById('spanid');
     var percent = document.getElementById('spanid1');
-
-
     var percentage;
-
     var sum = ip + se + cns + eeb + pce;
     var percentage = (sum / 500) * 100;
     add.innerHTML = sum;
@@ -21,7 +17,6 @@ function next() {
 
     if (percentage >= 80) {
         document.getElementById("Remarks").innerHTML = "<b>A</b>"
-
     }
     else if (percentage >= 70 && percentage <= 79) {
         document.getElementById("Remarks").innerHTML = "<b>B</b>"
@@ -29,15 +24,25 @@ function next() {
     else if (percentage >= 60 && percentage <= 69) {
         document.getElementById("Remarks").innerHTML = "<b>C</b>"
     }
-    else {
+    else if( percentage >= 35 && percentage <= 59 ) {
         document.getElementById("Remarks").innerHTML = "<b>D</b>"
+    }
+
+    else{
+        document.getElementById("Remarks").innerHTML= "<i>Unsuccessful Result Fail!!<i>"
     }
 
 }
 
 function res() {
-    document.getElementById('spanid').innerHTML = '--';
-    document.getElementById('spanid1').innerHTML = '--';
-    document.getElementById("Remarks").innerHTML='--';
+    document.getElementById('spanid').innerHTML = '';
+    document.getElementById('spanid1').innerHTML = '';
+    document.getElementById("Remarks").innerHTML='';
+    document.getElementById('inp1').value="";
+    document.getElementById('inp2').value="";
+    document.getElementById('inp3').value="";
+    document.getElementById('inp4').value="";
+    document.getElementById('inp5').value="";
+
 }
 
